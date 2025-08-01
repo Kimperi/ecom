864c24041f47fdc9dd5300175fdd23825e460178
Kimperi <baeljouhari@gmail.com>
2025-08-01 17:27:12 +0100

    added navbae

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,97 @@
-import React from "react";    
-import { assets } from '../assets/assets'
-import { NavLink } from "react-router-dom";
-
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="flex justify-between items-center py-4 font-medium">
+  return (
+    <div className="flex justify-between items-center py-5 font-medium bg-white shadow-sm px-10">
+      {/* Logo on the left */}
+      <div className="flex-1">
         <img src={assets.logo} className="w-36" alt="logo" />
-        <ul className="hidden md:flex gap-5 text-sm text-gray-700">
-           <NavLink className="hover:text-black transition-colors duration-300">
-            <p>Home</p>
-            </NavLink>
-        </ul>
-</div>
-    )
-}
+      </div>
 
-export default Navbar
+      {/* Navigation links in the center */}
+      <ul className="hidden md:flex gap-8 text-sm text-gray-700 justify-center flex-1 ">
+        <li>
+          <NavLink
+            to="/"
+            className="flex flex-col items-center gap-1 hover:text-black transition-colors duration-300"
+          >
+            <p>HOME</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/collection"
+            className="flex flex-col items-center gap-1 hover:text-black transition-colors duration-300"
+          >
+            <p>COLLECTION</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className="flex flex-col items-center gap-1 hover:text-black transition-colors duration-300"
+          >
+            <p>ABOUT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/contact"
+            className="flex flex-col items-center gap-1 hover:text-black transition-colors duration-300"
+          >
+            <p>CONTACT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className="flex items-center gap-5 flex-1 justify-end">
+        <img
+          src={assets.search_icon}
+          alt="search"
+          className="w-6 cursor-pointer"
+        />
+
+        <div className="group relative">
+          <img
+            src={assets.profile_icon}
+            alt="user"
+            className="w-5 cursor-pointer"
+          />
+          <div className="group-hover:block hidden absolute  dropdown-menu right-2 bottom -[-3px]">
+            <div className="flex flex-col gap-2 w-36 py-3 px-2 bg-gray-100 rounded-lg">
+              <p className="cursor-pointer hover:text-black transition-colors duration-300">
+                My Profile
+              </p>
+              <p className="cursor-pointer hover:text-black transition-colors duration-300">
+                Orders
+              </p>
+              <p className="cursor-pointer hover:text-black transition-colors duration-300">
+                Logout
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img
+            src={assets.cart_icon}
+            alt="cart"
+            className="w-5 cursor-pointer"
+          />
+          <p className="absolute top-[-10px] right-[-10px] w-4 h-4 bg-black rounded-full flex items-center justify-center text-white text-xs">
+            0
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
